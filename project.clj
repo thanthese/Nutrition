179e7abc6156d0bef86747bd669df0754e2ab3cb
Stephen Mann <thanthese@gmail.com>
2011-06-28 18:06:20 -0500

    added jdbc dependency

--- project.clj
+++ project.clj
@@ -1,6 +1,7 @@
 (defproject
   nutrition "0.1-dev"
   :description "Calculate ideal-ish nutrition"
-  :dependencies [[org.clojure/clojure "1.2.1"]]
+  :dependencies [[org.clojure/clojure "1.2.1"]
+                 [org.clojure/java.jdbc "0.0.2"]]
   :main nutrition.core
   :jvm-opts ["-Xmx1g" "-server"])
