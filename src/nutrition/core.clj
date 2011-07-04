ca5390884fc6972d9bc057416a523d882bd2dfaf
Stephen Mann <thanthese@gmail.com>
2011-07-04 16:11:44 -0500

    can find nutrients for, say, carrots

--- src/nutrition/core.clj
+++ src/nutrition/core.clj
@@ -1,7 +1,6 @@
 (ns nutrition.core
   (:use [clojure.pprint :only (pprint)])
   (:require [clojure.string :as str])
-  ;(:require [clojureql.core :as q])
   (:require [clojure.contrib.sql :as sql])
   (:require [nutrition.db-def :as db])
   (:gen-class))
@@ -17,16 +16,29 @@
   (do
     (doseq [row results]
       (println (interpose "|"(vals row))))
-    (print "Total: ")
-    (count results)))
+    (println "Total: " (count results))))
 
-(defn search-desc [search-term]
-  (query (format "select NDB_No, Long_Desc
-                 from food_description
-                 where lower(Long_Desc) like '%%%s%%'"
+(defn search-term [search-term]
+  (query (format "select food.NDB_No, grp.fdgrp_desc, food.Long_Desc
+                 from food_description as food,
+                   food_group as grp
+                 where food.fdgrp_cd = grp.fdgrp_cd
+                   and lower(food.Long_Desc) like '%%%s%%'"
                  (str/lower-case search-term))))
+
+(defn food-nutrients [ndb_no]
+  (query (format "select data.nutr_val,
+                   def.units, def.nutrdesc
+                 from nutrient_data as data, nutrient_definition as def
+                 where data.nutr_no = def.nutr_no
+                   and data.ndb_no = '%s'
+                   and data.nutr_val > 0
+                 order by data.nutr_val"
+                 ndb_no)))
 
 (defn -main [& args]
   (do
     (println "Show me the carrots!")
-    (pretty-table (search-desc "carrot"))))
+    (pretty-table (search-term "carrot"))
+    (println "Show me what's in 100g of raw carrots!")
+    (pretty-table (food-nutrients "11124"))))
