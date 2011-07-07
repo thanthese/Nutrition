3b0a08395cc0fa6e318206d69fb36e3a31aabd58
Stephen Mann <thanthese@gmail.com>
2011-07-06 22:17:11 -0500

    added first-crack at ideal diet

---
+++ src/nutrition/ideal_diet.clj
@@ -0,0 +1,67 @@
+(def dietary-guidelines
+  {:macronutrients
+   {{:nutrdesc "** alpha-linolenic acid" :units "g"   } 1.6
+    {:nutrdesc "** linoleic acid"        :units "g"   } 17
+    {:nutrdesc "Carbohydrate"            :units "g"   } 130
+    {:nutrdesc "Cholesterol"             :units "mg"  } 100
+    {:nutrdesc "Fiber"                   :units "g"   } 34
+    {:nutrdesc "Protein"                 :units "g"   } 55}
+   :minerals
+   {{:nutrdesc "Calcium"                 :units "mg"  } 1000
+    {:nutrdesc "Copper"                  :units "mcg" } 900
+    {:nutrdesc "Iron"                    :units "mg"  } 18
+    {:nutrdesc "Magnesium"               :units "mg"  } 400
+    {:nutrdesc "Phosphorus"              :units "mg"  } 700
+    {:nutrdesc "Potassium"               :units "mg"  } 4700
+    {:nutrdesc "Selenium"                :units "mcg" } 55
+    {:nutrdesc "Sodium"                  :units "mg"  } 1000
+    {:nutrdesc "Zinc"                    :units "mg"  } 11}
+   :vitamins
+   {{:nutrdesc "Choline"                 :units "mg"  } 550
+    {:nutrdesc "Folate"                  :units "mcg" } 400
+    {:nutrdesc "Niacin"                  :units "mg"  } 16
+    {:nutrdesc "Riboflavin"              :units "mg"  } 1.3
+    {:nutrdesc "Thiamin"                 :units "mg"  } 1.2
+    {:nutrdesc "Vitamin A"               :units "mcg" } 900
+    {:nutrdesc "Vitamin B12"             :units "mcg" } 2.4
+    {:nutrdesc "Vitamin B6"              :units "mg"  } 1.3
+    {:nutrdesc "Vitamin C"               :units "mg"  } 90
+    {:nutrdesc "Vitamin Dh"              :units "mcg" } 15
+    {:nutrdesc "Vitamin E"               :units "mg"  } 15
+    {:nutrdesc "Vitamin K"               :units "mcg" } 120}})
+
+(def self
+  {:macronutrients
+   {{:nutrdesc "** alpha-linolenic acid" :units "mg"  } 1600.0
+    {:nutrdesc "** linoleic acid"        :units "mg"  } 17000.0
+    {:nutrdesc "Carbohydrate"            :units "g"   } 130.0
+    {:nutrdesc "Fiber"                   :units "g"   } 38.0
+    {:nutrdesc "Protein"                 :units "g"   } 57}
+   :minerals
+   {{:nutrdesc "Calcium"                 :units "mg"  } 1000.0
+    {:nutrdesc "Chromium"                :units "mcg" } 35.0
+    {:nutrdesc "Copper"                  :units "mg"  } 0.9
+    {:nutrdesc "Flouride"                :units "mg"  } 4.0
+    {:nutrdesc "Iodine"                  :units "mcg" } 150.0
+    {:nutrdesc "Iron"                    :units "mg"  } 8.0
+    {:nutrdesc "Magnesium"               :units "mg"  } 400.0
+    {:nutrdesc "Manganese"               :units "mg"  } 2.3
+    {:nutrdesc "Molybdenum"              :units "mcg" } 45.0
+    {:nutrdesc "Phosphorus"              :units "mg"  } 700.0
+    {:nutrdesc "Selenium"                :units "mcg" } 55.0
+    {:nutrdesc "Zinc"                    :units "mg"  } 11.0}
+   :vitamins
+   {{:nutrdesc "Biotin"                  :units "mcg" } 30.0
+    {:nutrdesc "Choline"                 :units "mg"  } 550.0
+    {:nutrdesc "Folate"                  :units "mcg" } 400.0
+    {:nutrdesc "Niacin"                  :units "mg"  } 16.0
+    {:nutrdesc "Pantothenic Acid"        :units "mg"  } 5.0
+    {:nutrdesc "Riboflavin"              :units "mg"  } 1.3
+    {:nutrdesc "Thiamin"                 :units "mg"  } 1.2
+    {:nutrdesc "Vitamin A"               :units "IU"  } 3000.0
+    {:nutrdesc "Vitamin B12"             :units "mcg" } 2.4
+    {:nutrdesc "Vitamin B6"              :units "mg"  } 1.3
+    {:nutrdesc "Vitamin C"               :units "mg"  } 90.0
+    {:nutrdesc "Vitamin D"               :units "IU"  } 200.0
+    {:nutrdesc "Vitamin E"               :units "mg"  } 15.0
+    {:nutrdesc "Vitamin K"               :units "mcg" } 120.0}})
