b0d832f01204bd2006c3241b730eec5b9a8349f7
Stephen Mann <thanthese@gmail.com>
2011-07-20 22:14:13 -0500

    standard deviation

--- src/nutrition/the_algorithm.clj
+++ src/nutrition/the_algorithm.clj
@@ -1,17 +1,26 @@
 (ns nutrition.the-algorithm
+  (:require [clojure.contrib.math :as math])
   (:require [nutrition.query :as q])
   (:require [nutrition.ideal-diet :as i])
   (:require [nutrition.common-foods :as c]))
+
+(defn- average [nums]
+  (/ (apply + nums) (count nums)))
+
+(defn- standard-deviation [nums]
+  (let [mean (average nums)]
+    (math/sqrt (average (map (fn [n]
+                               (let [diff (- n mean)]
+                                 (* diff diff)))
+                             nums)))))
 
 (defn- score-against-active-male-ideal [recipe]
   (q/score-recipe recipe i/active-male-ideal))
 
 (defn- fitness [score]
-  (let [number (count score)
-        percents-sum (apply + (map :percent score))]
-    (/ percents-sum number)))
+  (standard-deviation (map :percent score)))
 
-(defn- quantities-fitness [quantities]
+(defn- quantities->fitness [quantities]
   (->> quantities
     flatten
     (apply q/recipe)
@@ -25,20 +34,27 @@
 
 (defn- branches [quantities]
   (map (fn [i]
-         (let [qtys (update-in quantities [i 1] inc)]
+         (let [qtys (update-in quantities [i 1] (partial + 20))]
            {:quantities qtys
-            :fitness (quantities-fitness qtys)}))
+            :fitness (quantities->fitness qtys)}))
        (range (count quantities))))
 
 (defn- fittest-branch [branches]
-  (apply max-key :fitness branches))
+  (do
+    (println "options: " (map (comp int :fitness) branches))
+    (apply min-key :fitness branches)))
 
-(defn- met-end-condition [branch]
-  (> (:fitness branch) 10))
+(defn- end-condition? [branch]
+  (some (fn [[ingredient amount]]
+          (> amount 5000))
+        (:quantities branch)))
 
 (defn evolve [& ndb_nos]
   (loop [quantities (initial-quantities ndb_nos)]
     (let [winning-br (fittest-branch (branches quantities))]
-      (if (met-end-condition winning-br)
+      (do
+        (println (:fitness winning-br))
+        (println (map second (:quantities winning-br)))
+        (if (end-condition? winning-br)
         winning-br
-        (recur (:quantities winning-br))))))
+        (recur (:quantities winning-br)))))))
